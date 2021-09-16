346abf7adef1285d771b5af3cfddd73a0e1440d0
Karstenveltman <karsten.veltman@coderclass.nl>
2021-09-16 16:11:05 +0200

    bijna alles werkt

--- AI learns to drive/car.js
+++ AI learns to drive/car.js
@@ -12,25 +12,61 @@ class Car {
     this.rays = []
     this.totalrays = 9;
     this.fov = 180;
-    this.carx1 = 0;
-    this.carx2 = 0;
-    this.carx3 = 0;
-    this.carx4 = 0;
-    this.cary1 = 0;
-    this.cary2 = 0;
-    this.cary3 = 0;
-    this.cary4 = 0;
+    this.carx1;
+    this.carx2;
+    this.carx3;
+    this.carx4;
+    this.cary1;
+    this.cary2;
+    this.cary3;
+    this.cary4;
     this.caralive = true;
+    this.carmoving = 0;
+    this.distances = [];
+    this.checkpoints = 0;
+    
+    this.brain = new NeuralNetwork(10, 10, 2);
+
     for (let i = 0; i < this.totalrays; i += 1) {
       this.rays.push(new Ray(this.xPos, this.yPos, this.angle));
     }
   }
   
-  display(walls) {
+  think(){
+    let inputs = [];
+    for (i = 0; i < 9; i++) {
+      inputs[i] = map(this.distances[i], 0, xSize, 0, 1);
+    }
+    inputs.push(this.carmoving);
+    
+    let output = this.brain.predict(inputs);
+
+    if (output[0] >= 0 && output[0] <= 0.5){
+      this.carmoving = 0;
+    }
+
+    if (output[0] > 0.5 && output[0] < 1){
+      this.carmoving = 1;
+    }
+
+    if (output[1] > 0 && output[1] < 0.33) {
+      this.angle += turnspeed;
+    }
+
+    if (output[1] > 0.33 && output[1] < 0.66) {
+    }
+
+    if (output[1] > 0.66 && output[1] < 1) {
+      this.angle -= turnspeed;
+    }
+  }
+
+  display() {
     if (this.caralive == true) {
-      fill(rgb);
+      fill(0);
       stroke(rgb)
       push()
+      fill(255,0, 0);
       translate(this.xPos, this.yPos)
       rectMode(CENTER)
       rotate(this.angle)
@@ -38,6 +74,12 @@ class Car {
       fill(255, 255, 255)
       rect(this.xLength/2.666, 0, this.xLength/5, this.yLength*0.8)
       pop()
+    }
+  }
+
+  raycalc(walls){
+    var distance;
+    if(this.caralive == true){
       for (let i = 0; i < this.rays.length; i++) {
         const ray = this.rays[i];
         ray.pos = createVector(this.xPos, this.yPos);
@@ -47,7 +89,7 @@ class Car {
         for (let i = 0; i <  walls.length; i++) {
           const point = ray.cast(walls[i]);
           if (point) {
-            let distance = p5.Vector.dist(ray.pos, point)
+            distance = p5.Vector.dist(ray.pos, point)
             if (distance < record) {
               record = distance;
               closest = point;
@@ -55,9 +97,9 @@ class Car {
           }
         }
         if (closest) {
-          stroke(255);
+          stroke(240);
           line(this.xPos, this.yPos, closest.x, closest.y);
-          circle(closest.x, closest.y, 5)
+          this.distances[i] = distance;
         }
       }
     }
@@ -104,24 +146,55 @@ class Car {
     }
   }
 
+  checkpointcalc(checkpoint) {
+    if (this.caralive == true) {
+      this.carx1 = this.xPos + cos(this.angle + atan(5/10)) * sqrt(125);
+      this.cary1 = this.yPos + sin(this.angle + atan(5/10)) * sqrt(125);
+
+      this.carx4 = this.xPos + cos(this.angle - atan(5/10)) * sqrt(125);
+      this.cary4 = this.yPos + sin(this.angle - atan(5/10)) * sqrt(125);
+
+      this.carx3 = this.xPos - cos(this.angle + atan(5/10)) * sqrt(125);
+      this.cary3 = this.yPos - sin(this.angle + atan(5/10)) * sqrt(125);
+
+      this.carx2 = this.xPos - cos(this.angle - atan(5/10)) * sqrt(125);
+      this.cary2 = this.yPos - sin(this.angle - atan(5/10)) * sqrt(125);
+      const x1 = checkpoint.point1.x;
+      const y1 = checkpoint.point1.y;
+      const x2 = checkpoint.point2.x;
+      const y2 = checkpoint.point2.y;
+
+      const x3 = this.carx1;
+      const y3 = this.cary1;
+      const x4 = this.carx2;
+      const y4 = this.cary2;
+      const x5 = this.carx3;
+      const y5 = this.cary3;
+      const x6 = this.carx4;
+      const y6 = this.cary4;
+
+      const t1 = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+      const u1 = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+      const t2 = ((x1 - x5) * (y5 - y6) - (y1 - y5) * (x5 - x6)) / ((x1 - x2) * (y5 - y6) - (y1 - y2) * (x5 - x6));
+      const u2 = ((x2 - x1) * (y1 - y5) - (y2 - y1) * (x1 - x5)) / ((x1 - x2) * (y5 - y6) - (y1 - y2) * (x5 - x6));
+      
+      if (0 < t1 && t1 < 1 && u1 > 0 && u1 < 1) {
+        this.checkpoints++
+        console.log(this.checkpoints)
+
+      }
+      else if (0 < t2 && t2 < 1 && u2 > 0 && u2 < 1){
+        this.checkpoints++
+      }
+    }
+  }
+
   move() {
     if (this.caralive == true) {
-      if (keyIsDown(this.up)){
-        this.xPos -= cos(this.angle) * this.speed;
-        this.yPos -= sin(this.angle) * this.speed;
-      } 
-      else if (keyIsDown(this.down)){
+      if (this.carmoving == 1){
         this.xPos += cos(this.angle) * this.speed;
         this.yPos += sin(this.angle) * this.speed;
       } 
-
-      if (keyIsDown(68)){
-        this.angle += turnspeed;
-      } 
-
-      else if (keyIsDown(65)){
-        this.angle -= turnspeed;
-      }
     }
   }
 }
--- AI learns to drive/draw.js
+++ AI learns to drive/draw.js
@@ -1,36 +1,81 @@
-var xSize = 600;
-var ySize = 350;
+var xSize = 1200;
+var ySize = 600;
 var rgb = 0;
 var bc = 200;
 var turnspeed = 3;
-var walls = []
-carAlive = true
+var walls = [];
+var cars = [];
+var checkpoints = [];
+var TOTAL = 100;
+var cardeadcount = 0;
+var alldead = false;
 
 function setup() {
   angleMode(DEGREES)
-  Car1 = new Car(20, 10, 150, 150, 0, 3, 83, 87);
-  createCanvas(xSize, ySize);  
-  walls.push(new Wall(200, 225, 400, 225));
-  walls.push(new Wall(100, 350/3, 100, 350/2*2));
-  walls.push(new Wall(100, 350/3, 500, 350/3));
-  walls.push(new Wall(500, 350/3, 500, 350/2*2));
-  walls.push(new Wall(100, 350/2*2, 500, 350/2*2));
+  for (i = 0; i < TOTAL; i ++) {
+    cars[i] = new Car(20, 10, 350, 150, 0, 4, 83, 87);
+  }
+  createCanvas(xSize, ySize); 
+  
+  walls.push(new Wall(100, 100, 500, 100)); 
+  walls.push(new Wall(200, 200, 500, 200));
+  walls.push(new Wall(100, 200, 100, 100));
+  walls.push(new Wall(700, 300, 700, 500));
+  walls.push(new Wall(600, 300, 600, 400));
+  walls.push(new Wall(500, 100, 700, 300));
+  walls.push(new Wall(500, 200, 600, 300));
+  walls.push(new Wall(600, 400, 300, 300));
+  walls.push(new Wall(700, 500, 300, 400));
+  walls.push(new Wall(300, 300, 200, 200));
+  walls.push(new Wall(300, 400, 100, 200));
+
+  checkpoints.push(new Checkpoint(500, 100, 500, 200));
+  checkpoints.push(new Checkpoint(600, 300, 700, 300));
+  checkpoints.push(new Checkpoint(600, 400, 700, 500));
+  checkpoints.push(new Checkpoint(300, 300, 300, 400));
+  checkpoints.push(new Checkpoint(200, 100, 200, 200));
+  checkpoints.push(new Checkpoint(200, 200, 100, 200));
 }
 
 function draw() {
   background(bc);
+
   for (let wall of walls) {
     wall.display();
   }
-  Car1.display(walls);
-  for (let i = 0; i <  walls.length; i++) {
-    Car1.cornercalc(walls[i]);
+  for (let checkpoint of checkpoints) {
+    checkpoint.display();
   }
-  Car1.move();
+  for (car of cars) {
+    car.display();
+    car.raycalc(walls);
+    
+    for (let i = 0; i <  walls.length; i++) {
+      car.cornercalc(walls[i]);
+    }
+
+    car.checkpointcalc(checkpoints[car.checkpoints]);
+    
+    car.think();
+    car.move()
+  }
+
+  for (car in cars) {
+    if (car.caralive == false) {
+      cardeadcount++
+    }
+  }
+
+  if (cardeadcount == TOTAL) {
+    alldead = true;
+  }
+
   if (keyIsDown(82)){
-    Car1.caralive = true;
-    Car1.xPos = 150;
-    Car1.yPos = 150;
-    Car1.angle = 0;
+    for (car of cars){
+      car.caralive = true;
+      car.xPos = 350;
+      car.yPos = 150;
+      car.angle = 0;
+    }
   } 
 }
